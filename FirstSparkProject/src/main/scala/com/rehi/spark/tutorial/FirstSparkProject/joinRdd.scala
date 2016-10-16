df6dd338a3ba57d4142f17aa99bc847fee0a283b
DEEPESH REHI <rehi.deepesh@gmail.com>
2016-10-16 21:41:45 +0530

    parseString method Included by BALA
    BALAMANIKANDAN is an scala expert in Cognizant Technology Solutions,
    You can take help regarding any scala problems by sending mail at: balamanikandant2016@gmail.com

--- FirstSparkProject/src/main/scala/com/rehi/spark/tutorial/FirstSparkProject/joinRdd.scala
+++ FirstSparkProject/src/main/scala/com/rehi/spark/tutorial/FirstSparkProject/joinRdd.scala
@@ -12,9 +12,29 @@ object joinRdd {
 
     val readFile2 = sc.textFile("test.txt", 3).map { x => x.split(",") }.map { x => (x(0), (x(1), x(2))) }
 
-    def sl: (Any => String) = (arg: Any) => arg.toString().replaceAll("\\(", " ").replaceAll("\\)", "").replaceAll(" ", "").trim()
+      def sl: (Any => String) = (arg: Any) => arg.toString()
+      .replaceAll("\\(", " ")
+      .replaceAll("\\)", "")
+      .replaceAll(" ", "")
+      .trim()
 
-    val finalTable = readFile1.join(readFile2).values.map { x => sl(x) }
+    //edited by BALAMANIKANDAN T balamanikandant2016@gmail.com
+
+    def parseString(msg: Any): String = {
+      msg match {
+        case tuple @ (a: Any, b: Any) => {
+          a.toString() + "," + b.toString().replaceAll("[()]", "").trim()
+        }
+        case _ => ""
+      }
+    }
+
+
+   //edited by BALAMANIKANDAN T balamanikandant2016@gmail.com
+
+    //val finalTable = readFile1.join(readFile2).values.map { x => sl(x) }
+
+   val finalTable = readFile1.join(readFile2).values.map { x => parseString(x) }
 
     val readFinalTable = finalTable.collect().toList.map { println }
 
